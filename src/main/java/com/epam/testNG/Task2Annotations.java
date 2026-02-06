62f0bd27cb4d72a96f30b7e81984a38480f2739b
atkarisoujanya <atkari_soujanya@epam.com>
2026-02-06 19:13:39 +0530

    TestNG task1.1 done

---
+++ src/main/java/com/epam/testNG/Task2Annotations.java
@@ -0,0 +1,46 @@
+package com.epam.testNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Task2Annotations {
+    WebDriver driver;
+    WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(2000));
+    @BeforeMethod
+    public void setUp(){
+        driver=new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+    }
+
+    @Test
+    public void testMethod1(){
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void testMethod2(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String readyState =js.executeScript("return document.readyState").toString();
+        Assert.assertEquals(readyState, "complete");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
