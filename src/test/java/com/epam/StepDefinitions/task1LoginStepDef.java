db60823eba14f1ef11a376263f57ed0649ad7776
atkarisoujanya <atkari_soujanya@epam.com>
2026-02-08 15:07:27 +0530

    task3 dataprovider done

--- src/test/java/com/epam/StepDefinitions/task1LoginStepDef.java
+++ src/test/java/com/epam/StepDefinitions/task1LoginStepDef.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -17,7 +18,7 @@ public class task1LoginStepDef {
     WebDriverWait wait;
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        driver = new EdgeDriver();
+        driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
         loginPage = new SeleTask1LoginPage(driver);
     }
