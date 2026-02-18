bbcae853b6a7e856ead7be6ec0e9ed40e0fb10ca
atkarisoujanya <atkari_soujanya@epam.com>
2026-02-18 15:48:16 +0530

    Jenkinsfile added

--- src/test/java/com/epam/testrunner/Runner.java
+++ src/test/java/com/epam/testrunner/Runner.java
@@ -5,10 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-            features = "src/test/resources/task1Login.feature",
+            features = "src/test/resources/features/task1Login.feature",
             glue = "com.epam.StepDefinitions",
             plugin = {"pretty", "html:target/cucumber-reports.html",
-                    "junit:target/cucumber-results.xml"
             }
     )
     public class Runner extends AbstractTestNGCucumberTests {
