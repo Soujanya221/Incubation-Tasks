bae6330aa410eb2fb00e5e89a627dfc6e1b89708
atkarisoujanya <atkari_soujanya@epam.com>
2026-02-18 14:11:00 +0530

    Annotationsclass updated with headless mode

--- src/main/java/com/epam/testNG/Task2Annotations.java
+++ src/main/java/com/epam/testNG/Task2Annotations.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -18,7 +19,16 @@ public class Task2Annotations {
     WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(2000));
     @BeforeMethod
     public void setUp(){
-        driver=new EdgeDriver();
+        EdgeOptions options = new EdgeOptions();
+
+        // Headless mode
+        options.addArguments("--headless=new");
+
+        // Recommended additional arguments
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        driver=new EdgeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://google.com");
     }
