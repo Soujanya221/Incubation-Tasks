030650f14599426e9fb2bced55675b7c304116da
atkarisoujanya <atkari_soujanya@epam.com>
2026-02-25 10:33:37 +0530

    cashbox problem done

---
+++ src/main/java/com/epam/collections/Task10/Buyer.java
@@ -0,0 +1,19 @@
+package com.epam.collections.Task10;
+
+public class Buyer {
+
+    private char name;
+
+    public Buyer(char name) {
+        this.name = name;
+    }
+
+    public char getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(name);
+    }
+}
